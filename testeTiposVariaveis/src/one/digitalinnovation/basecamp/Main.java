a519978dc6ac81066d3cf5c4cfe45f196990a9d4
CintiaKarolliny <cintiakarolliny.9@gmail.com>
2022-07-18 18:12:11 -0300

    Projeto sobre programação em Java. Aprendendo sobre variáveis, tipos de dados e operadores matemáticos

---
+++ testeTiposVariaveis/src/one/digitalinnovation/basecamp/Main.java
@@ -0,0 +1,58 @@
+package one.digitalinnovation.basecamp;
+
+/* Classe de exemplo para o exercício da Aula 2 de Variáveis, Tipos de Dado e Operadores aritméticos */
+
+public class Main {
+    public static void main(String[] args) {
+
+        byte b1 = 10;
+        byte b2 = 20;
+
+        short s1 = 20000;
+        //short s2 = 40000;
+
+        //int i1 = -10000000000;
+        int i2 = 20500;
+
+        long l1 = 100000000000000L; // ou l
+        long l2 = 200400500520000L; // ou l
+
+        // Variáveis reais
+
+        //float f1 = 4.5;
+        float f2 = 10.60f; // ou F
+
+        double d1 = 85.69;
+        double d2 = 99.84d; // ou D
+
+        // textuais
+
+        char c1 = 'W';
+        //char c2 = 'Tw';
+        char c3 = '\u0057';
+
+        String st1 = "Fulano";
+        String st2 = "Cicrano";
+        String st3 = "sdf fghj yg r DFGHJ HBNC $%#@";
+
+        // String dt1 = "09/02/1981"; nn usar data como string
+
+        boolean bo1 = true;
+        boolean bo2 = false;
+
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(s1);
+        System.out.println(i2);
+        System.out.println(l1);
+        System.out.println(l2);
+        System.out.println(c1);
+        System.out.println(c3);
+        System.out.println(st1);
+        System.out.println(st2);
+        System.out.println(st3);
+        System.out.println(bo1);
+        System.out.println(bo2);
+
+    }
+}
